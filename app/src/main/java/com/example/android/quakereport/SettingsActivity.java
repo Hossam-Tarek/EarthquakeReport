a95fd78efdd42e88ac90ee4f747c0f35ed6e558b
Hossam Tarek <hossamtarek250@gmail.com>
2018-07-25 09:24:40 +0200

    Add order by preference to settings

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.quakereport;
 
 import android.content.SharedPreferences;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
@@ -28,6 +29,9 @@ public class SettingsActivity extends AppCompatActivity {
 
             Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
             bindPreferenceSummaryToValue(minMagnitude);
+
+            Preference orderBy = findPreference(getString(R.string.settings_order_by_key));
+            bindPreferenceSummaryToValue(orderBy);
         }
 
         private void bindPreferenceSummaryToValue(Preference preference) {
@@ -43,7 +47,16 @@ public class SettingsActivity extends AppCompatActivity {
         @Override
         public boolean onPreferenceChange(Preference preference, Object o) {
             String value = o.toString();
-            preference.setSummary(value);
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int preferenceIndex = listPreference.findIndexOfValue(value);
+                if (preferenceIndex >= 0) {
+                    CharSequence[] lables = listPreference.getEntries();
+                    preference.setSummary(lables[preferenceIndex]);
+                }
+            } else {
+                preference.setSummary(value);
+            }
             return true;
         }
     }
